c3c59728bdc026c4f45478e4831dfc810f67dc58
Abdusaid Umarov <abdusaidumarov@Abdusaids-MacBook-Pro.local>
2023-10-15 08:03:14 +0500

    fix/fixed client name empty edit mode

--- src/modules/orders-report/useModule.ts
+++ src/modules/orders-report/useModule.ts
@@ -25,17 +25,17 @@ export default function useModule() {
       }
     const editOrder = async () => {
     // console.log('get: ',selectedData.value)//batch_inputs[], batch_quantity, error
-        ordersStore.newOrderState.client_id = selectedData.value.client.id
-        ordersStore.newOrderState.receipt = {'id':selectedData.value.receipt.id, 'name':selectedData.value.receipt.name}  
-        ordersStore.newOrderState.date = selectedData.value.date
-        ordersStore.newOrderState.amount = selectedData.value.amount
+        ordersStore.newOrderState.client_id = selectedData.value.client?.id
+        ordersStore.newOrderState.receipt = {'id':selectedData.value.receipt?.id, 'name':selectedData.value.receipt.name}  
+        ordersStore.newOrderState.date = selectedData.value?.date
+        ordersStore.newOrderState.amount = selectedData.value?.amount
 
-        ordersStore.newOrderState.batch_inputs = selectedData.value.batch_inputs
-        ordersStore.newOrderState.batch_quantity = selectedData.value.batch_quantity
+        ordersStore.newOrderState.batch_inputs = selectedData.value?.batch_inputs
+        ordersStore.newOrderState.batch_quantity = selectedData.value?.batch_quantity
         ordersStore.newOrderState.error = parseFloat(((selectedData.value.error - 1) * 100).toPrecision(3))
 
-        ordersStore.newOrderState.receipt_id = selectedData.value.receipt.id
-        ordersStore.newOrderState.receipt_name = selectedData.value.receipt.name
+        ordersStore.newOrderState.receipt_id = selectedData.value?.receipt.id
+        ordersStore.newOrderState.receipt_name = selectedData.value?.receipt?.name
         // ---------------------------------------------------------------------
         ordersStore.setIsEditState(true)
         ordersStore.setDeleteOrderId(route.params.id)
