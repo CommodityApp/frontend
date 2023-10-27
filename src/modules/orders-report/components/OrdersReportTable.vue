181b04a638a71fcab8ad86294070baa6a95de43e
Abdusaid Umarov <abdusaidumarov@Abdusaids-MacBook-Pro.local>
2023-10-27 15:33:18 +0500

    fixed minor issues

--- src/modules/orders-report/components/OrdersReportTable.vue
+++ src/modules/orders-report/components/OrdersReportTable.vue
@@ -1,23 +1,25 @@
 <script setup lang="ts">
 defineProps<{
-  selectedOrder: any,
-  selectedBatch: any,
-  index: number,
-  selectedReceipt: any,
-  selectedPrice: any,
-  selectedOveralAmountError: any,
-  selectedOveralAmount: any
-}>()
+  selectedOrder: any;
+  selectedBatch: any;
+  index: number;
+  selectedReceipt: any;
+  selectedPrice: any;
+  selectedOveralAmountError: any;
+  selectedOveralAmount: any;
+}>();
 </script>
 <template>
   <div
-    v-if="selectedOrder.length" 
+    v-if="selectedOrder.length"
     class="relative overflow-x-auto bg-white shadow-md sm:rounded-lg px-5 py-6 mt-4"
   >
     <div class="font-bold text-xl flex align-middle items-center">
-      Замес {{ index + 1 }} - 
-      <span class="bg-purple-100 text-purple-800 text-sm font-medium ml-2 mr-2 px-2.5 py-0.5 rounded">
-        <b>{{selectedBatch}} кг</b>
+      Замес {{ index + 1 }} -
+      <span
+        class="bg-purple-100 text-purple-800 text-sm font-medium ml-2 mr-2 px-2.5 py-0.5 rounded"
+      >
+        <b>{{ selectedBatch }} кг</b>
       </span>
     </div>
 
@@ -27,36 +29,35 @@ defineProps<{
           <tr>
             <th scope="col" class="px-6 py-3">Сырье</th>
             <th scope="col" class="px-6 py-3">Концентрация (кг/т)</th>
-            <th scope="col" class="px-6 py-3">Прайс</th>
             <th scope="col" class="px-6 py-3">С погрешностью</th>
-            <th scope="col" class="px-6 py-3">Без погрешность</th>
+            <th scope="col" class="px-6 py-3">Без погрешностью</th>
+            <th scope="col" class="px-6 py-3">Прайс</th>
           </tr>
         </thead>
         <tbody>
-          <tr 
+          <tr
             class="bg-white border-b text-left text-gray-900 hover:bg-gray-50"
-            v-for="order, i in selectedOrder"
+            v-for="(order, i) in selectedOrder"
             :key="i"
           >
-            <td class="px-6 py-4">{{order.receipt_raw?.raw?.name}}</td>
-            <td class="px-6 py-4">{{order.receipt_raw?.ratio}}</td>
-            <td class="px-6 py-4">{{order.price}}</td>
-            <td class="px-6 py-4">{{order.calculated_amount_with_error[index]}}</td>
-            <td class="px-6 py-4">{{order.calculated_amount[index]}}</td>
+            <td class="px-6 py-4">{{ order.receipt_raw?.raw?.name }}</td>
+            <td class="px-6 py-4">{{ order.receipt_raw?.ratio }}</td>
+            <td class="px-6 py-4">
+              {{ order.calculated_amount_with_error[index] }}
+            </td>
+            <td class="px-6 py-4">{{ order.calculated_amount[index] }}</td>
+            <td class="px-6 py-4">{{ order.price }}</td>
           </tr>
         </tbody>
         <tr>
-            <th scope="col" class="px-6 py-3">Итого:</th>
-            <th class="px-6 py-4">{{selectedReceipt.concentration}}</th>
-            <th class="px-6 py-4">{{selectedPrice}}</th>
-            <th class="px-6 py-4">{{selectedOveralAmountError[index]}}</th>
-            <th class="px-6 py-4">{{selectedOveralAmount[index]}}</th>
+          <th scope="col" class="px-6 py-3">Итого:</th>
+          <th class="px-6 py-4">{{ selectedReceipt.concentration }}</th>
+          <th class="px-6 py-4">{{ selectedPrice }}</th>
+          <th class="px-6 py-4">{{ selectedOveralAmountError[index] }}</th>
+          <th class="px-6 py-4">{{ selectedOveralAmount[index] }}</th>
         </tr>
       </table>
-      
     </div>
   </div>
-  <div v-else class="text-2xl text-center p-16 text-gray-300">
-    Нет данных
-  </div>
+  <div v-else class="text-2xl text-center p-16 text-gray-300">Нет данных</div>
 </template>
--- src/modules/receipts-add/components/ReceiptsAddForm.vue
+++ src/modules/receipts-add/components/ReceiptsAddForm.vue
@@ -4,42 +4,42 @@ import { WarningIcon } from "@/app/assets/svg";
 import LoaderOverlay from "@/app/components/LoaderOverlay.vue";
 
 const props = defineProps<{
-  isLoading: boolean,
-  rawsData: any,
-  singleReceipt: any,
-  queryType: any,
-  animalTypes: any
-}>()
+  isLoading: boolean;
+  rawsData: any;
+  singleReceipt: any;
+  queryType: any;
+  animalTypes: any;
+}>();
 
 const emit = defineEmits<{
-  onSaveReceipt: [receiptData: any]
-}>()
+  onSaveReceipt: [receiptData: any];
+}>();
 
-const visibleAlert = ref<boolean>()
-const rate = ref<number>()
-const code = ref<string>()
-const name = ref<string>()
-const unit = ref<string>()
-const producer_name = ref<string>()
-const concentration = ref<number>()
-const selectedAnimalTypes = ref({})
-const animal_type_id = ref<number>()
+const visibleAlert = ref<boolean>();
+const rate = ref<number>();
+const code = ref<string>();
+const name = ref<string>();
+const unit = ref<string>();
+const producer_name = ref<string>();
+const concentration = ref<number>();
+const selectedAnimalTypes = ref({});
+const animal_type_id = ref<number>();
 const receipt_raws = ref([
   {
-    raw_id:null, 
-    ratio:null
-  }
-])
+    raw_id: null,
+    ratio: null,
+  },
+]);
 
 const removeRawInput = () => {
-  receipt_raws.value.pop()
-}
+  receipt_raws.value.pop();
+};
 const addRawInput = () => {
   receipt_raws.value.push({
     raw_id: null,
-    ratio: null
-  })
-}
+    ratio: null,
+  });
+};
 
 const saveReceipt = () => {
   let newReceipt = {
@@ -50,72 +50,87 @@ const saveReceipt = () => {
     animal_type_id: animal_type_id.value,
     producer_name: producer_name.value,
     concentration: concentration.value,
-    receipt_raws: receipt_raws.value.filter((item) => {return item.raw_id !== null})
-  }
-  emit("onSaveReceipt", newReceipt)
-}
+    receipt_raws: receipt_raws.value.filter((item) => {
+      return item.raw_id !== null;
+    }),
+  };
+  emit("onSaveReceipt", newReceipt);
+};
 
 const sumOfRatio = computed(() => {
-  return receipt_raws.value.reduce((acc, item) => {
-    return acc = parseFloat(acc + (item.ratio == undefined ? 0 : item.ratio))
-  }, 0).toFixed(2)
-})
-
-watch(receipt_raws, () => {
-  sumOfRatio.value != concentration.value ? visibleAlert.value = true : visibleAlert.value = false
-},{
-  deep: true
-})
-
-watch(() => props.singleReceipt, () => {
-  if( props.singleReceipt.code ){
-    code.value = props.singleReceipt.code 
-    name.value = props.singleReceipt.name
-    rate.value = props.singleReceipt.rate
-    unit.value = props.singleReceipt.unit
-    selectedAnimalTypes.value = props.singleReceipt.animal_type?.parent_id
-    animal_type_id.value = props.singleReceipt.animal_type?.id
-    producer_name.value = props.singleReceipt.producer_name
-    concentration.value = props.singleReceipt.concentration
-    
-    props.singleReceipt.receipt_raws.forEach((item) => {
-      receipt_raws.value.push({
-        raw_id: item.raw.id,
-        ratio: parseFloat(item.ratio)
-      })
-    })
-  }
-  if(props.queryType) {
-    code.value = null
-    name.value = null
-  }
-  //clearing null value
-  receipt_raws.value.shift()
-}, {
-  deep: true
-})
+  return receipt_raws.value
+    .reduce((acc, item) => {
+      return (acc = parseFloat(
+        acc + (item.ratio == undefined ? 0 : item.ratio)
+      ));
+    }, 0)
+    .toFixed(2);
+});
 
 watch(
-    () => selectedAnimalTypes.value, 
-    () => {
-    selectedAnimalTypes.value ? '' : animal_type_id.value = null
-})
+  receipt_raws,
+  () => {
+    sumOfRatio.value != concentration.value
+      ? (visibleAlert.value = true)
+      : (visibleAlert.value = false);
+  },
+  {
+    deep: true,
+  }
+);
+
+watch(
+  () => props.singleReceipt,
+  () => {
+    if (props.singleReceipt.code) {
+      code.value = props.singleReceipt.code;
+      name.value = props.singleReceipt.name;
+      rate.value = props.singleReceipt.rate;
+      unit.value = props.singleReceipt.unit;
+      selectedAnimalTypes.value = props.singleReceipt.animal_type?.parent_id;
+      animal_type_id.value = props.singleReceipt.animal_type?.id;
+      producer_name.value = props.singleReceipt.producer_name;
+      concentration.value = props.singleReceipt.concentration;
+
+      props.singleReceipt.receipt_raws.forEach((item) => {
+        receipt_raws.value.push({
+          raw_id: item.raw.id,
+          ratio: parseFloat(item.ratio),
+        });
+      });
+    }
+    if (props.queryType) {
+      code.value = null;
+      name.value = null;
+    }
+    //clearing null value
+    receipt_raws.value.shift();
+  },
+  {
+    deep: true,
+  }
+);
+
+watch(
+  () => selectedAnimalTypes.value,
+  () => {
+    selectedAnimalTypes.value ? "" : (animal_type_id.value = null);
+  }
+);
 const computedAnimalTypes = computed(() => {
   return props.animalTypes.filter((item) => {
-    return item.id == selectedAnimalTypes.value
-  })
-})
-
+    return item.id == selectedAnimalTypes.value;
+  });
+});
 
 const isEdit = computed(() => {
-  return props.singleReceipt?.code != null && !props.queryType ? true : false
-})
-
+  return props.singleReceipt?.code != null && !props.queryType ? true : false;
+});
 </script>
 <template>
   <div class="flex flex-row justify-between py-2 w-full">
     <div class="self-center text-2xl font-bold leading-7">
-      <span v-if="isEdit">Редактирование</span> 
+      <span v-if="isEdit">Редактирование</span>
       <span v-else-if="queryType">Дублирование</span>
       <span v-else>Добавление</span>
       рецептуры
@@ -126,14 +141,17 @@ const isEdit = computed(() => {
         @click="saveReceipt()"
         class="flex flex-row bg-[#7000FF] disabled:bg-[#6f00ff41] cursor-pointer disabled:cursor-not-allowed text-white rounded-[1rem] py-[0.4rem] px-[0.9rem]"
       >
-      <span v-if="isEdit">Изменить</span>
-      <span v-else-if="queryType">Дублировать</span>
-      <span v-else>Сохранить</span>
-    </button>
+        <span v-if="isEdit">Изменить</span>
+        <span v-else-if="queryType">Дублировать</span>
+        <span v-else>Сохранить</span>
+      </button>
     </div>
   </div>
   <LoaderOverlay v-if="isLoading" />
-  <div v-else class="relative overflow-x-auto bg-white shadow-md sm:rounded-lg px-5 py-6">
+  <div
+    v-else
+    class="relative overflow-x-auto bg-white shadow-md sm:rounded-lg px-5 py-6"
+  >
     <form>
       <div class="grid md:grid-cols-2 md:gap-6">
         <div class="relative z-0 w-full group">
@@ -150,7 +168,7 @@ const isEdit = computed(() => {
           <label
             for="code"
             class="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-[#7000FF] peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
-            >Код сырья</label
+            >Код рецепта</label
           >
         </div>
         <div class="relative z-0 w-full group">
@@ -166,7 +184,7 @@ const isEdit = computed(() => {
           <label
             for="name"
             class="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-[#7000FF] peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
-            >Название сырья</label
+            >Наименовение рецепта</label
           >
         </div>
 
@@ -210,18 +228,18 @@ const isEdit = computed(() => {
             >Вид животных
           </label>
           <select
-            v-model="selectedAnimalTypes" 
+            v-model="selectedAnimalTypes"
             id="animal_types"
             class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-[#7000FF] peer"
           >
             <option></option>
-            <option 
-              v-for="animal, index in animalTypes"
+            <option
+              v-for="(animal, index) in animalTypes"
               :key="index"
               :value="animal.id"
             >
-            {{animal.name}} ( {{animal.children.length}} )
-          </option>
+              {{ animal.name }} ( {{ animal.children.length }} )
+            </option>
           </select>
         </div>
 
@@ -231,7 +249,7 @@ const isEdit = computed(() => {
             class="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-[#7000FF] peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
             >Тип животных
           </label>
-          
+
           <select
             :disabled="!selectedAnimalTypes"
             id="floating_animal_type"
@@ -239,20 +257,16 @@ const isEdit = computed(() => {
             class="block px-2.5 pb-2.5 pt-4 disabled:cursor-not-allowed disabled:border-red-200 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-[#7000FF] peer"
           >
             <option></option>
-            <option 
-              v-for="selectedAnimal, index in computedAnimalTypes[0]?.children"
+            <option
+              v-for="(selectedAnimal, index) in computedAnimalTypes[0]
+                ?.children"
               :key="index"
               :value="selectedAnimal.id"
-              >
-              {{selectedAnimal.name}}
+            >
+              {{ selectedAnimal.name }}
             </option>
           </select>
         </div>
-
-
-
-
-
 
         <div class="relative z-0 w-full group">
           <input
@@ -276,14 +290,14 @@ const isEdit = computed(() => {
             name="concentration"
             id="concentration"
             v-model="concentration"
-            :class="{'border-red-700':visibleAlert}"
+            :class="{ 'border-red-700': visibleAlert }"
             class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-[#7000FF] peer"
             placeholder=" "
             required
           />
           <label
             for="concentration"
-            :class="{'text-red-700':visibleAlert}"
+            :class="{ 'text-red-700': visibleAlert }"
             class="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-[#7000FF] peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
             >Концентрация</label
           >
@@ -292,48 +306,52 @@ const isEdit = computed(() => {
     </form>
 
     <!--  -->
-    <div v-if="visibleAlert" class="flex mt-4 p-2 text-sm text-red-800 rounded-lg bg-red-50" role="alert">
-      <WarningIcon class="w-4 h-4 self-center gap-2 mx-2"/>
+    <div
+      v-if="visibleAlert"
+      class="flex mt-4 p-2 text-sm text-red-800 rounded-lg bg-red-50"
+      role="alert"
+    >
+      <WarningIcon class="w-4 h-4 self-center gap-2 mx-2" />
       <span>
-        Cумма значений <u>Корма</u> должно быть равно <b>(=)</b> значения <u>Концентрации</u>. 
-        <br/>
-        <b>Калькуляция:</b> <i>(Корма)</i> <b>{{ sumOfRatio }} != {{ concentration }}</b> <i>(Концентрации)</i>
+        Cумма значений <u>Корма</u> должно быть равно <b>(=)</b> значения
+        <u>Концентрации</u>.
+        <br />
+        <b>Калькуляция:</b> <i>(Корма)</i>
+        <b>{{ sumOfRatio }} != {{ concentration }}</b> <i>(Концентрации)</i>
       </span>
     </div>
     <!-- <hr class="h-px my-8 bg-gray-200 border-0" /> -->
     <div class="inline-flex items-center justify-center mt-4 w-full">
-      <hr class="w-full h-px my-8 bg-gray-200 border-0">
-      <span class="absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-[10%]">
+      <hr class="w-full h-px my-8 bg-gray-200 border-0" />
+      <span
+        class="absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-[10%]"
+      >
         Добавьте сырье
       </span>
     </div>
 
-
-    <div 
-      v-for="_, index in receipt_raws" 
+    <div
+      v-for="(_, index) in receipt_raws"
       class="grid md:grid-cols-3 md:gap-6 my-6"
     >
       <div class="relative z-0 w-full group">
-          <label
+        <label
           :for="`raw${index}`"
-            class="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-[#7000FF] peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
-            >Клиент
-          </label>
-          
-          <select 
-            v-model="receipt_raws[index]['raw_id']"
-            id="`raw${index}`"
-            class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-[#7000FF] peer"
-          >
-            <option></option>
-            <option  
-              v-for="raw, index in rawsData"
-              :key="index"
-              :value="raw.id">
-              {{ raw.name }}
-            </option>
-          </select>
-        </div>
+          class="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-[#7000FF] peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+          >Клиент
+        </label>
+
+        <select
+          v-model="receipt_raws[index]['raw_id']"
+          id="`raw${index}`"
+          class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-[#7000FF] peer"
+        >
+          <option></option>
+          <option v-for="(raw, index) in rawsData" :key="index" :value="raw.id">
+            {{ raw.name }}
+          </option>
+        </select>
+      </div>
 
       <div class="relative z-0 w-full group">
         <input
@@ -341,18 +359,17 @@ const isEdit = computed(() => {
           name="unit_rato"
           :id="`unit_ratio${index}`"
           v-model="receipt_raws[index]['ratio']"
-          :class="{'border-red-700':visibleAlert}"
+          :class="{ 'border-red-700': visibleAlert }"
           class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-[#7000FF] peer"
           placeholder=" "
         />
         <label
           :for="`unit_ratio${index}`"
-          :class="{'text-red-700':visibleAlert}"
+          :class="{ 'text-red-700': visibleAlert }"
           class="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-[#7000FF] peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
           >Корма (кг/т)
         </label>
       </div>
-      
     </div>
 
     <div>
