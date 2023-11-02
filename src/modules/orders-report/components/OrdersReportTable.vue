b77990404990ee15d608223715a8716ee9696688
Abdusaid Umarov <abdusaidumarov@Abdusaids-MacBook-Pro.local>
2023-11-02 21:51:15 +0500

    added showhide consumption in order module

--- src/modules/orders-report/components/OrdersReportTable.vue
+++ src/modules/orders-report/components/OrdersReportTable.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { EyeIcon } from "@/app/assets/svg";
-import { ref } from "vue";
-defineProps<{
+import { ref, computed } from "vue";
+const props = defineProps<{
   selectedOrder: any;
   selectedBatch: any;
   index: number;
@@ -11,9 +11,11 @@ defineProps<{
   selectedOveralAmount: any;
 }>();
 
+const isConsentr = ref(false);
 const isCalcAmountErr = ref(false);
 const isCalcAmount = ref(false);
 const isPrice = ref(false);
+const isConsumpt = ref(false);
 
 const toggleColumn = (columnName) => {
   switch (columnName) {
@@ -29,8 +31,29 @@ const toggleColumn = (columnName) => {
       isPrice.value = true;
       return;
     }
+    case "isConsentr": {
+      isConsentr.value = true;
+      return;
+    }
+    case "isConsumpt": {
+      isConsumpt.value = true;
+      return;
+    }
   }
 };
+
+const computedConsumption = computed(() => {
+  return props.selectedOrder.reduce((acc, item) => {
+    return (acc =
+      acc +
+      parseFloat(
+        (
+          (item.receipt_raw?.ratio * item.price) /
+          props.selectedReceipt.concentration
+        ).toFixed(3)
+      ));
+  }, 0);
+});
 </script>
 <template>
   <div
@@ -51,7 +74,14 @@ const toggleColumn = (columnName) => {
         <thead class="text-xs text-gray-700 uppercase bg-gray-50">
           <tr>
             <th scope="col" class="p-3">Сырье</th>
-            <th scope="col" class="p-3">Концентр. (кг/т)</th>
+            <th scope="col" class="p-3" :class="[isConsentr ? 'hidden' : '']">
+              <div class="flex items-center">
+                Концентр. (кг/т)
+                <button @click="toggleColumn('isConsentr')">
+                  <EyeIcon class="w-4 h-4 mx-2" />
+                </button>
+              </div>
+            </th>
             <th
               scope="col"
               class="p-3"
@@ -80,9 +110,17 @@ const toggleColumn = (columnName) => {
                 </button>
               </div>
             </th>
-            <th scope="col" class="p-3">Расх. (сум/кг)</th>
+            <th scope="col" class="p-3" :class="[isConsumpt ? 'hidden' : '']">
+              <div class="flex items-center">
+                Расх. (сум/кг)
+                <button @click="toggleColumn('isConsumpt')">
+                  <EyeIcon class="w-4 h-4 mx-2" />
+                </button>
+              </div>
+            </th>
           </tr>
         </thead>
+
         <tbody>
           <tr
             class="bg-white border-b text-left text-gray-900 hover:bg-gray-50"
@@ -90,9 +128,11 @@ const toggleColumn = (columnName) => {
             :key="i"
           >
             <td class="p-3">{{ order.receipt_raw?.raw?.name }}</td>
-            <td class="p-3">{{ order.receipt_raw?.ratio }}</td>
+            <td class="p-3" :class="[isConsentr ? 'hidden' : '']">
+              {{ order.receipt_raw?.ratio }}
+            </td>
             <td class="p-3" :class="[isCalcAmountErr ? 'hidden' : '']">
-              {{ order.calculated_amount_with_error[index] }}
+              {{ order.calculated_amount_with_error[index].toFixed(3) }}
             </td>
             <td class="p-3" :class="[isCalcAmount ? 'hidden' : '']">
               {{ order.calculated_amount[index] }}
@@ -100,7 +140,7 @@ const toggleColumn = (columnName) => {
             <td class="p-3" :class="[isPrice ? 'hidden' : '']">
               {{ order.price }}
             </td>
-            <td class="p-3">
+            <td class="p-3" :class="[isConsumpt ? 'hidden' : '']">
               {{
                 (
                   (order.receipt_raw?.ratio * order.price) /
@@ -110,9 +150,12 @@ const toggleColumn = (columnName) => {
             </td>
           </tr>
         </tbody>
+
         <tr>
           <th scope="col" class="p-3">Итого:</th>
-          <th class="p-3">{{ selectedReceipt.concentration }}</th>
+          <th class="p-3" :class="[isConsentr ? 'hidden' : '']">
+            {{ selectedReceipt.concentration }}
+          </th>
           <th class="p-3" :class="[isCalcAmountErr ? 'hidden' : '']">
             {{ selectedOveralAmountError[index] }}
           </th>
@@ -122,7 +165,9 @@ const toggleColumn = (columnName) => {
           <th class="p-3" :class="[isPrice ? 'hidden' : '']">
             <!-- {{ selectedPrice }} -->
           </th>
-          <th class="p-3"></th>
+          <th class="p-3" :class="[isConsumpt ? 'hidden' : '']">
+            {{ computedConsumption }}
+          </th>
         </tr>
       </table>
     </div>
